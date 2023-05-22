7dca5ea047f702c99bf05e1902cba7367a172015
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-05-22 13:12:57 +0200

    added init Project

---
+++ Project/Client/pw2i_09/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Svg from "./svg.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Svg></Svg>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Project/Client/pw2i_09/src/svg.js
@@ -0,0 +1,5 @@
+function SVG(props) {
+  return <p></p>;
+}
+
+export default SVG;
