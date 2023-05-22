664ddbca8c837cff001f4df194e6fe3350c39f32
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-05-22 15:06:35 +0200

    added svg image

--- Project/Client/pw2i_09/src/svg.js
+++ Project/Client/pw2i_09/src/svg.js
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import img from "./assets/map-tg.svg";
+import "./svg.css";
+import SVGInjection from "@iconfu/svg-inject";
+
 function SVG(props) {
-  return <p></p>;
+  useEffect(() => {
+    SVGInject.setOptions({ makeIdsUnique: false });
+    const elMapTg = document.getElementById("tg-map");
+    SVGInject(elMapTg);
+  });
+
+  return (
+    <>
+      <img id="tg-map" src={img} />
+    </>
+  );
 }
 
 export default SVG;
