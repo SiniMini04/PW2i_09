72114060d5677537d5f85d4e69923319849b492a
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-05-26 10:27:24 +0200

    added nav bar data

--- Project/Client/pw2i_09/src/navbar.js
+++ Project/Client/pw2i_09/src/navbar.js
@@ -3,50 +3,51 @@ import "./navbar.css";
 import React from "react";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import axios from 'axios';
+import axios from "axios";
 
-async function getAbst(){
-  const url = 'http://localhost:3030/api/todos/abstim';
-  let votinglist = [];
-  let data =  await axios.get(url).then( (response) => response.data.art);  
+let abstimmung =
+  "Objektkredit von Fr. 16'300'000 für den Erweiterungsbau der Kantonsschule Frauenfeld";
+
+async function getAbst() {
+  const url = "http://localhost:3030/api/todos/abstim";
+  let data = await axios.get(url).then((response) => response.data.art);
   console.log(data);
   return data;
 }
 
-
+function getAbstimmung() {
+  console.log("Send");
+  return abstimmung;
+}
 
 function NAV(props) {
-  
-
-  const [basicOpen, setBasicOpen] = useState(true);
-  const [basicCollapse1, setBasicCollapse1] = useState(true);
-  const [basicCollapse2, setBasicCollapse2] = useState(false);
-  //const [votinglist, changeVoting] = useState(null)
-
- 
-  
-
-  //getvotinglist();
-
-  useEffect(() => {//
-    return 
-    () => {
-      let votinglist = getAbst().then((response) => {
-        console.log(response.data);
-      });
-      console.log("grrrr2", votinglist);
-      //let votinglist = [1,1,2,3];
-    
-      votinglist = [1,2,3];
+  useEffect(() => {
+    return () => {
       const ul = document.querySelector("ul");
+      let votinglist = [];
+      getAbst().then((res) => {
+        votinglist = res;
 
-      for (let i = 0; i < votinglist.length; i++) {
-        const li = document.createElement("li");
-        ul.appendChild(li);
-        li.innerHTML = votinglist[i];
-      }
+        console.log(votinglist);
+
+        for (let i = 0; i < votinglist.length; i++) {
+          const li = document.createElement("li");
+          const link = document.createElement("a");
+          li.appendChild(link);
+          ul.appendChild(li);
+          link.innerHTML = votinglist[i];
+          link.addEventListener("click", () => {
+            sendDataToBackend(votinglist[i]);
+          });
+        }
+      });
     };
   }, []);
+
+  const sendDataToBackend = (data) => {
+    console.log("Sending data to backend:", data);
+    abstimmung = data;
+  };
 
   return (
     <>
@@ -57,5 +58,5 @@ function NAV(props) {
   );
 }
 
-
 export default NAV;
+export { getAbstimmung };
--- Project/Server/todo/todo.controller.js
+++ Project/Server/todo/todo.controller.js
@@ -1,208 +1,184 @@
-import { response } from 'express';
-import {getAll, create, update} from './todo.model.js';
+import { response } from "express";
+import { getAll, create, update } from "./todo.model.js";
 
 let globTodo = null;
 
-async function getTodos(request, response){
-    if(globTodo != null){
-        response.json(globTodo);
-    }else{
-        const todos = await getAll();
-        globTodo = todos;
-        response.json(todos); 
-    }
-              //Warum wird der return in das attribut return geschrieben?
+async function getTodos(request, response) {
+  if (globTodo != null) {
+    response.json(globTodo);
+  } else {
+    const todos = await getAll();
+    globTodo = todos;
+    response.json(todos);
+  }
 }
 
-
-
-async function createTodo(request, response){
-    const todo = await create(request.body);
-    response.json(todo);
+async function createTodo(request, response) {
+  const todo = await create(request.body);
+  response.json(todo);
 }
 
-async function updateTodo(request, response){
-    await update(request.params.id, request.body).then((todo) => { response.json(todo); }).catch((error) => { response.status(404).json({ message: error });});
+async function updateTodo(request, response) {
+  await update(request.params.id, request.body)
+    .then((todo) => {
+      response.json(todo);
+    })
+    .catch((error) => {
+      response.status(404).json({ message: error });
+    });
 }
 
-async function getFor(req, res){ //Gibt alles zurück was irgend wo den param enthält.
-    const abst = req.params.abstimung;
+async function getFor(req, res) {
+  //Gibt alles zurück was irgend wo den param enthält.
+  const abst = req.params.abstimung;
 
-
-    let to = await getAll();
-    let newTo = [];
-    for(let i = 0; i < to.length; i++){
-        
-        if(Object.values(to[i]).includes(abst)){
-            console.log("Gefunden");
-            newTo.push(to[i]);
-        }
+  let to = await getAll();
+  let newTo = [];
+  for (let i = 0; i < to.length; i++) {
+    if (Object.values(to[i]).includes(abst)) {
+      console.log("Gefunden");
+      newTo.push(to[i]);
     }
-    console.log(to);
-    console.log(abst);
-    res.send(newTo);
-
+  }
+  console.log(to);
+  console.log(abst);
+  res.send(newTo);
 }
 
-async function getGemHoech(req,res){
-    let te = await getAll();
-    let hoechst = [];
-    
+async function getGemHoech(req, res) {
+  let te = await getAll();
+  let hoechst = [];
 
-    for(let i = 0; i < te.length; i++){
-        te.sort((a,b) => {
-            return b.stimmbeteiligung - a.stimmbeteiligung;
-        });
+  for (let i = 0; i < te.length; i++) {
+    te.sort((a, b) => {
+      return b.stimmbeteiligung - a.stimmbeteiligung;
+    });
+  }
+  for (let i = 0; i < te.length; i++) {
+    hoechst.push(te[i].stimmbeteiligung);
+  }
+  let highest10 = [];
+  for (let i = 0; i < 10; i++) {
+    highest10.push(hoechst[i]);
+  }
 
-    }
-    for(let i = 0; i < te.length; i++){
-        hoechst.push(te[i].stimmbeteiligung)
-
-    }
-    let highest10 = [];
-    for(let i = 0; i < 10; i++){
-        highest10.push(hoechst[i]);
-    }
-
-
-    console.log(hoechst);
-    res.send(highest10);
+  console.log(hoechst);
+  res.send(highest10);
 }
 
-async function getJaNein(req, res){
-    let par = req.params.abst;
-    
-    let to = await getAll();
-    let newTo = [];
+async function getJaNein(req, res) {
+  let par = req.params.abst;
 
-    for(let i = 0; i < to.length; i++){
-        
-        if(Object.values(to[i]).includes(par)){
-            console.log("Gefunden");
-            newTo.push(to[i]);
-        }
+  let to = await getAll();
+  let newTo = [];
+
+  for (let i = 0; i < to.length; i++) {
+    if (Object.values(to[i]).includes(par)) {
+      console.log("Gefunden");
+      newTo.push(to[i]);
     }
-    
-    
-    console.log(newTo);
-    let obj = {
-        ja_stimmen: 0,
-        nein_stimmen: 0
-    }
+  }
 
-    for(let a = 0; a < newTo.length; a++){
+  console.log(newTo);
+  let obj = {
+    ja_stimmen: 0,
+    nein_stimmen: 0,
+  };
 
-        let ja_stimmen = newTo[a].ja_stimmen;
-        let nein_stimmen = newTo[a].nein_stimmen;
+  for (let a = 0; a < newTo.length; a++) {
+    let ja_stimmen = newTo[a].ja_stimmen;
+    let nein_stimmen = newTo[a].nein_stimmen;
 
-        obj.ja_stimmen += ja_stimmen;
-        obj.nein_stimmen += nein_stimmen;
-
-    }
-    console.log(newTo);
-    res.send(obj);
-
+    obj.ja_stimmen += ja_stimmen;
+    obj.nein_stimmen += nein_stimmen;
+  }
+  console.log(newTo);
+  res.send(obj);
 }
-async function getguelungueleer(req, res){
-    let par = req.params.abst;
-    
-    let to = await getAll();
-    let newTo = [];
+async function getguelungueleer(req, res) {
+  let par = req.params.abst;
 
-    for(let i = 0; i < to.length; i++){
-        
-        if(Object.values(to[i]).includes(par)){
-            console.log("Gefunden");
-            newTo.push(to[i]);
-        }
+  let to = await getAll();
+  let newTo = [];
+
+  for (let i = 0; i < to.length; i++) {
+    if (Object.values(to[i]).includes(par)) {
+      console.log("Gefunden");
+      newTo.push(to[i]);
     }
-    
-    
-    console.log(newTo);
-    let obj = {
-        gueltige_stimmen: 0,
-        ungueltige_stimmen: 0,
-        leere_stimmen: 0
-    }
+  }
 
-    for(let a = 0; a < newTo.length; a++){
+  console.log(newTo);
+  let obj = {
+    gueltige_stimmen: 0,
+    ungueltige_stimmen: 0,
+    leere_stimmen: 0,
+  };
 
-        let gueltige_stimmen = newTo[a].gueltige_stimmen;
-        let ungueltige_stimmen = newTo[a].ungueltige_stimmen;
-        let leere_stimmen = newTo[a].leere_stimmen;
+  for (let a = 0; a < newTo.length; a++) {
+    let gueltige_stimmen = newTo[a].gueltige_stimmen;
+    let ungueltige_stimmen = newTo[a].ungueltige_stimmen;
+    let leere_stimmen = newTo[a].leere_stimmen;
 
-        obj.gueltige_stimmen += gueltige_stimmen;
-        obj.ungueltige_stimmen += ungueltige_stimmen;
-        obj.leere_stimmen += leere_stimmen;
-
-    }
-    console.log(newTo);
-    res.send(obj);
-
+    obj.gueltige_stimmen += gueltige_stimmen;
+    obj.ungueltige_stimmen += ungueltige_stimmen;
+    obj.leere_stimmen += leere_stimmen;
+  }
+  console.log(newTo);
+  res.send(obj);
 }
 
 async function abfrage(req, res) {
-    let par = req.params.diagram;
-    switch(par){
-        case "abstim":
-            const todo = await getAll();
-            const data = [];
-            console.log(todo.length);
-            for(let i = 0; i < todo.length; i++) {
+  let par = req.params.diagram;
+  switch (par) {
+    case "abstim":
+      const todo = await getAll();
+      const data = [];
+      console.log(todo.length);
+      for (let i = 0; i < todo.length; i++) {
+        let item = todo[i].vorlage_bezeichnung;
+        if (!data.includes(item)) {
+          data.push(item);
+        }
+      }
 
-                
-                
-                let item = todo[i].vorlageart_bezeichnung;
-                if(!data.includes(item)){
-                    data.push(item);
-                }
-                
-            }
+      console.log(data);
+      res.json({ art: data });
 
-                    
-                
-            
-            
-            
-            console.log(data);
-            res.json({art: data});
-            
-            break;
-        case "tooltip":
+      break;
+    case "tooltip":
+      const todos = await getAll();
+      let data2 = [];
 
-            
-            const todos = await getAll();
-            let data2 = []
-            
-            for(let i = 0; i < todos.length; i++){
-                let name = todos[i].gemeinde_name;
-                let stimmberechtigte = todos[i].stimmberechtigte;
-                let eingelegte_stimmzettel = todos[i].eingelegte_stimmzettel;
-                let leere_stimmen = todos[i].leere_stimmen;
-                let ungueltige_stimmen = todos[i].ungueltige_stimmen;
-                let gueltige_stimmen = todos[i].gueltige_stimmen;
-                let ja_stimmen = todos[i].ja_stimmen;
-                let nein_stimmen = todos[i].nein_stimmen;
-                let stimmbeteiligung = todos[i].stimmbeteiligung; //9
+      for (let i = 0; i < todos.length; i++) {
+        let name = todos[i].gemeinde_name;
+        let stimmberechtigte = todos[i].stimmberechtigte;
+        let eingelegte_stimmzettel = todos[i].eingelegte_stimmzettel;
+        let leere_stimmen = todos[i].leere_stimmen;
+        let ungueltige_stimmen = todos[i].ungueltige_stimmen;
+        let gueltige_stimmen = todos[i].gueltige_stimmen;
+        let ja_stimmen = todos[i].ja_stimmen;
+        let nein_stimmen = todos[i].nein_stimmen;
+        let stimmbeteiligung = todos[i].stimmbeteiligung; //9
 
-                let obj = {};
-                
-                obj = {
-                    "name" : name,
-                    "stimmberechtigte": stimmberechtigte,
-                    "eingelegte_stimmzettel": eingelegte_stimmzettel,
-                    "leere_stimmen" : leere_stimmen,
-                    "ungueltige_stimmen" : ungueltige_stimmen,
-                    "gueltige_stimmen" : gueltige_stimmen,
-                    "ja_stimmen" : ja_stimmen,
-                    "neinstimmen" : nein_stimmen,
-                    "stimmbeteiligung" : stimmbeteiligung
-                }
-                data2.push(obj);
-                
-            //case "dia1":
+        let obj = {};
 
-                /*
+        obj = {
+          name: name,
+          stimmberechtigte: stimmberechtigte,
+          eingelegte_stimmzettel: eingelegte_stimmzettel,
+          leere_stimmen: leere_stimmen,
+          ungueltige_stimmen: ungueltige_stimmen,
+          gueltige_stimmen: gueltige_stimmen,
+          ja_stimmen: ja_stimmen,
+          neinstimmen: nein_stimmen,
+          stimmbeteiligung: stimmbeteiligung,
+        };
+        data2.push(obj);
+
+        //case "dia1":
+
+        /*
                 - Ja nein im kanton
 
                 - Diagramm wahl zwischen 
@@ -216,30 +192,26 @@ async function abfrage(req, res) {
                 - gültig
                     */
 
-                //break;
-            }
-            console.log(data2);
-            res.send(data2);
-           
+        //break;
+      }
+      console.log(data2);
+      res.send(data2);
 
+      res.send("d2");
+      break;
 
-
-
-            res.send('d2');
-        break;
-
-
-        case "dia1":
-
-            break;
-    }
+    case "dia1":
+      break;
+  }
 }
 
-
-
-
-
-
-
-
-export { getTodos, createTodo, updateTodo, abfrage, getFor, getJaNein, getguelungueleer, getGemHoech };
+export {
+  getTodos,
+  createTodo,
+  updateTodo,
+  abfrage,
+  getFor,
+  getJaNein,
+  getguelungueleer,
+  getGemHoech,
+};
